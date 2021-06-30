7bae0194b6c14a9f621128007cdf68e02affe69e
ssookie <hyunsue77@gmail.com>
2021-07-01 08:56:27 +0900

    컴포넌트 스캔

---
+++ src/main/java/hola/springbasic/Controller/MemberController.java
@@ -0,0 +1,17 @@
+package hola.springbasic.Controller;
+
+import hola.springbasic.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    // private final MemberService memberSerivce = new MemberService();
+    private final MemberService memberService;
+
+    @Autowired // 생성자 호출시 Autowired → 스프링이 스프링 컨테이너에 있는 memberService를 가져와서 연결시켜줌.
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
