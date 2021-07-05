269dffd25225288df9ceeb8ab883aa49ee46873b
ssookie <hyunsue77@gmail.com>
2021-07-05 09:01:35 +0900

    간단... 웹 MVC 개발

---
+++ src/main/java/hola/springbasic/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package hola.springbasic.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
---
+++ src/main/java/hola/springbasic/Controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hola.springbasic.Controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
